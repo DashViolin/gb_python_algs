e02877b62a692bc7e7fd44f88a48a17a22f518c5
dash <dasha_erminski@mail.ru>
2022-04-02 01:13:02 +0300

    dz07 - fixed some remarks

--- dz07/dz_1.py
+++ dz07/dz_1.py
@@ -5,41 +5,45 @@
 
 from unittest import TestCase
 from random import randint
+from random import choice
 
 
-# MergeSort (from https://www.programiz.com/dsa/merge-sort)
-def merge_sort(array):
-    if len(array) <= 1:
-        return None
-    mid = len(array) // 2
-    left = array[:mid]
-    right = array[mid:]
-    merge_sort(left)
-    merge_sort(right)
-    i = j = k = 0
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            array[k] = left[i]
-            i += 1
-        else:
-            array[k] = right[j]
-            j += 1
-        k += 1
-    while i < len(left):
-        array[k] = left[i]
-        i += 1
-        k += 1
-    while j < len(right):
-        array[k] = right[j]
-        j += 1
-        k += 1
-
-
-def get_median(items: list):
-    if items:
-        merge_sort(items)
-        return items[len(items) // 2]
+def quickselect_median(array):
+    if array:
+        return quickselect(array, len(array) // 2)
     return None
+
+
+def quickselect(array, k):
+    if len(array) == 1 and k == 0:
+        return array[0]
+    pivot = choice(array)
+    lows = [el for el in array if el < pivot]
+    highs = [el for el in array if el > pivot]
+    pivots = [el for el in array if el == pivot]
+    if k < len(lows):
+        return quickselect(lows, k)
+    elif k < len(lows) + len(pivots):
+        return pivots[0]
+    else:
+        return quickselect(highs, k - len(lows) - len(pivots))
+
+
+def pick_pivot(array):
+    def chunked(items, chunk_size):
+        return [items[i:i + chunk_size] for i in range(0, len(items), chunk_size)]
+
+    if not array:
+        return None
+    if len(array) < 5:
+        array = sorted(array)
+        return array[len(array) // 2]
+    chunks = chunked(array, 5)
+    full_chunks = [chunk for chunk in chunks if len(chunk) == 5]
+    sorted_groups = [sorted(chunk) for chunk in full_chunks]
+    medians = [chunk[2] for chunk in sorted_groups]
+    median_of_medians = quickselect_median(medians)
+    return median_of_medians
 
 
 class Tests(TestCase):
@@ -48,7 +52,7 @@ class Tests(TestCase):
         length = 2 * m + 1
         rands = [randint(-9, 9) for _ in range(length)]
         print(rands)
-        self.assertEqual(list(sorted(rands[:]))[m], get_median(rands[:]))
+        self.assertEqual(list(sorted(rands[:]))[m], quickselect_median(rands[:]))
 
     def test_empty(self):
-        self.assertEqual(None, get_median([]))
+        self.assertEqual(None, quickselect_median([]))
--- dz07/dz_2.py
+++ dz07/dz_2.py
@@ -1,9 +1,46 @@
 # https://leetcode.com/problems/replace-words/solution/
+
+# class Solution:
+#     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+#         tokens = sentence.split()
+#         for index, _ in enumerate(tokens):
+#             for root in dictionary:
+#                 if tokens[index].startswith(root):
+#                     tokens[index] = root
+#         return ' '.join(tokens)
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = None
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        curr.word = word
+
+    def replace(self, word):
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                break
+            curr = curr.children[char]
+            if curr.word:
+                return curr.word
+        return word
+
+
 class Solution:
-    def replaceWords(self, dictionary: list[str], sentence: str) -> str:
-        tokens = sentence.split()
-        for index, _ in enumerate(tokens):
-            for root in dictionary:
-                if tokens[index].startswith(root):
-                    tokens[index] = root
-        return ' '.join(tokens)
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie = Trie()
+        for word in dictionary:
+            trie.insert(word)
+        return " ".join(map(trie.replace, sentence.split()))
